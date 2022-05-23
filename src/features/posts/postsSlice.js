63a3db7c5f00ed104f13ab762cb507ee858fe9b2
Walid Amar <walidamarpro@gmail.com>
2022-05-22 23:41:32 -0400

    Last step with errors

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,10 +1,24 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
 const initialState = {
   posts: [],
   status: 'idle',
   error: null
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
+
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  async (initialPost) => {
+    const response = await client.post('/fakeApi/posts', initialPost)
+    return response.data
+  }
+)
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -49,6 +63,23 @@ const postsSlice = createSlice({
       }
     },
   },
+
+  /// Extra reducers
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  }
 })
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
